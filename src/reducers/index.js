1ad637247fcea3ccdcfd50dfffd6846b8b70ac92
Jason Benesch <jasonbenesch@gmail.com>
2017-04-10 14:37:46 -0700

    add settings reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import movies from './movies';
+import settings from './settings';
 
 const rootReducer = combineReducers({
   movies,
+  settings,
 });
 
 export default rootReducer;
